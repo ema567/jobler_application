40215ee9decc3063d0d1adf56ec5d5d29e773b37
eman rezk <emann.rezkk@gmail.com>
2024-06-18 05:33:44 +0300

    generated_plugin_registrant generated_plugins  GeneratedPluginRegistrant pubspec.yaml  pubspec.lock generated_plugin_registrant  generated_plugins.cmake

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,26 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import cloud_firestore
-import file_selector_macos
-import firebase_app_check
-import firebase_auth
-import firebase_core
-import firebase_messaging
-import firebase_storage
-import google_sign_in_ios
-import rive_common
-import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
-  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-  FLTFirebaseAppCheckPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAppCheckPlugin"))
-  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
-  FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
-  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
-  RivePlugin.register(with: registry.registrar(forPlugin: "RivePlugin"))
-  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
