34a259943f6b84601bcf0f2bf215d24d0e8c0f8e
eman rezk <emann.rezkk@gmail.com>
2024-06-19 08:57:21 +0300

    login.dart

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -5,7 +5,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-
 void main() {
   runApp(const Login());
 }
@@ -29,197 +28,198 @@ class _LoginState extends State<Login> {
         backgroundColor: Colors.white,
         body: SingleChildScrollView(
           child: Form(
-              key: _formKey,
-              child: Stack(children: [
-                Column(children: [
-                  SizedBox(height: 10),
-                  Text(
+            key: _formKey,
+            child: Stack(children: [
+              Column(children: [
+                SizedBox(height: 10),
+                Text(
+                  "Login",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    height: 1,
+                    letterSpacing: 1,
+                  ),
+                ),
+                Image.asset("assets/images/login.png"),
+                Text(
+                  "login to your account",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    height: 1,
+                    wordSpacing: 1,
+                  ),
+                ),
+                SizedBox(height: 15),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple[50],
+                    borderRadius: BorderRadius.circular(66),
+                  ),
+                  width: 266,
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: TextFormField(
+                    controller: email,
+                    decoration: InputDecoration(
+                      icon: Icon(
+                        Icons.email_outlined,
+                        color: Colors.deepPurple[300],
+                      ),
+                      hintText: "Your Email :",
+                      hintStyle: TextStyle(color: Colors.black),
+                      border: InputBorder.none,
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter your email";
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                SizedBox(height: 10),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple[50],
+                    borderRadius: BorderRadius.circular(66),
+                  ),
+                  width: 266,
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: TextFormField(
+                    controller: password,
+                    obscureText: false,
+                    decoration: InputDecoration(
+                      suffix: Icon(
+                        Icons.visibility,
+                        color: Colors.deepPurple[300],
+                      ),
+                      icon: Icon(
+                        Icons.lock,
+                        color: Colors.deepPurple[300],
+                        size: 19,
+                      ),
+                      hintText: "Password :",
+                      hintStyle: TextStyle(color: Colors.black),
+                      border: InputBorder.none,
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter your password";
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      try {
+                        final credential = await FirebaseAuth.instance
+                            .signInWithEmailAndPassword(
+                                email: email.text, password: password.text);
+                        Navigator.of(context).pushReplacementNamed("/Home");
+                      } on FirebaseAuthException catch (e) {
+                        String errorMessage = '';
+
+                        if (e.code == 'user-not-found') {
+                          errorMessage = 'No user found for that email.';
+
+                          AwesomeDialog(
+                            context: context,
+                            dialogType: DialogType.error,
+                            animType: AnimType.rightSlide,
+                            title: 'Error',
+                            desc: 'the email not correct',
+                            btnOkOnPress: () {},
+                          ).show();
+                        } else if (e.code == 'wrong-password') {
+                          errorMessage =
+                              'Wrong password provided for that user.';
+                        }
+                        {
+                          AwesomeDialog(
+                            context: context,
+                            dialogType: DialogType.error,
+                            animType: AnimType.rightSlide,
+                            title: 'Error',
+                            desc: 'the password not correct',
+                            btnOkOnPress: () {},
+                          ).show();
+                        }
+                      }
+                    } else {
+                      print("Form is not valid");
+                    }
+                  },
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(Colors.deepPurple[300]),
+                    padding: MaterialStateProperty.all(
+                        EdgeInsets.symmetric(horizontal: 106, vertical: 10)),
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(27))),
+                  ),
+                  child: Text(
                     "Login",
                     style: TextStyle(
-                      color: Colors.black,
+                      color: Colors.white,
                       fontSize: 25,
-                      fontWeight: FontWeight.bold,
-                      height: 1,
-                      letterSpacing: 1,
                     ),
                   ),
-                  Image.asset("assets/images/login.png"),
-                  Text(
-                    "login to your account",
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      height: 1,
-                      wordSpacing: 1,
-                    ),
-                  ),
-                  SizedBox(height: 15),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.deepPurple[50],
-                      borderRadius: BorderRadius.circular(66),
-                    ),
-                    width: 266,
-                    padding: EdgeInsets.symmetric(horizontal: 16),
-                    child: TextFormField(
-                      controller: email,
-                      decoration: InputDecoration(
-                        icon: Icon(
-                          Icons.email_outlined,
-                          color: Colors.deepPurple[300],
-                        ),
-                        hintText: "Your Email :",
-                        hintStyle: TextStyle(color: Colors.black),
-                        border: InputBorder.none,
-                      ),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return "Please enter your email";
-                        }
-                        return null;
-                      },
-                    ),
-                  ),
-                  SizedBox(height: 10),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.deepPurple[50],
-                      borderRadius: BorderRadius.circular(66),
-                    ),
-                    width: 266,
-                    padding: EdgeInsets.symmetric(horizontal: 16),
-                    child: TextFormField(
-                      controller: password,
-                      obscureText: false,
-                      decoration: InputDecoration(
-                        suffix: Icon(
-                          Icons.visibility,
-                          color: Colors.deepPurple[300],
-                        ),
-                        icon: Icon(
-                          Icons.lock,
-                          color: Colors.deepPurple[300],
-                          size: 19,
-                        ),
-                        hintText: "Password :",
-                        hintStyle: TextStyle(color: Colors.black),
-                        border: InputBorder.none,
-                      ),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return "Please enter your password";
-                        }
-                        return null;
-                      },
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: () async {
-                      if (_formKey.currentState!.validate()) {
-                        try {
-                          final credential = await FirebaseAuth.instance
-                              .signInWithEmailAndPassword(
-                                  email: email.text, password: password.text);
-                          Navigator.of(context).pushReplacementNamed("/Home");
-                        } on FirebaseAuthException catch (e) {
-                          String errorMessage = '';
-
-                          if (e.code == 'user-not-found') {
-                            errorMessage = 'No user found for that email.';
-
-                            AwesomeDialog(
-                              context: context,
-                              dialogType: DialogType.error,
-                              animType: AnimType.rightSlide,
-                              title: 'Error',
-                              desc: 'the email not correct',
-                              btnOkOnPress: () {},
-                            ).show();
-                          } else if (e.code == 'wrong-password') {
-                            errorMessage =
-                                'Wrong password provided for that user.';
-                          }
-                          {
-                            AwesomeDialog(
-                              context: context,
-                              dialogType: DialogType.error,
-                              animType: AnimType.rightSlide,
-                              title: 'Error',
-                              desc: 'the password not correct',
-                              btnOkOnPress: () {},
-                            ).show();
-                          }
-                        }
-                      } else {
-                        print("Form is not valid");
-                      }
-                    },
-                    style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all(Colors.deepPurple[300]),
-                      padding: MaterialStateProperty.all(
-                          EdgeInsets.symmetric(horizontal: 106, vertical: 10)),
-                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(27))),
-                    ),
-                    child: Text(
-                      "Login",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 25,
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 25),
-                  SizedBox(
-                    width: 299,
-                    child: Row(
-                      children: [
-                        Expanded(
-                            child: Divider(
-                          thickness: 0.6,
-                          color: Colors.deepPurple[300],
-                        )),
-                        Text(
-                          "OR",
-                          style: TextStyle(
-                            color: Colors.deepPurple[300],
-                          ),
-                        ),
-                        Expanded(
-                            child: Divider(
-                          thickness: 0.6,
-                          color: Colors.deepPurple[300],
-                        )),
-                      ],
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                ),
+                SizedBox(height: 25),
+                SizedBox(
+                  width: 299,
+                  child: Row(
                     children: [
-                      Text("Don't have an accout? "),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.pushNamed(context, "Signup");
-                        },
-                        child: Text(
-                          "Sign up",
-                          style: TextStyle(fontWeight: FontWeight.bold),
+                      Expanded(
+                          child: Divider(
+                        thickness: 0.6,
+                        color: Colors.deepPurple[300],
+                      )),
+                      Text(
+                        "OR",
+                        style: TextStyle(
+                          color: Colors.deepPurple[300],
                         ),
                       ),
+                      Expanded(
+                          child: Divider(
+                        thickness: 0.6,
+                        color: Colors.deepPurple[300],
+                      )),
                     ],
                   ),
-                  Positioned(
-                    left: 0,
-                    child: Image.asset(
-                      "assets/images/main_top.png",
-                      width: 111,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Don't have an accout? "),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushNamed(context, "Signup");
+                      },
+                      child: Text(
+                        "Sign up",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
                     ),
-                  ),
-                ]),
-              ])),
+                  ],
+                ),
+              ]),
+              Positioned(
+                left: 0,
+                child: Image.asset(
+                  "assets/images/main_top.png",
+                  width: 111,
+                ),
+              ),
+            ]),
+          ),
         ),
       ),
     );
